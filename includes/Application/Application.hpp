5243eaa20312409d4b50da977419602d5d644e4f
matthiaspn <matthias.petitjean@epitech.eu>
2022-06-27 21:24:55 +0200

    feat: Adding the begin of the documentation

--- includes/Application/Application.hpp
+++ includes/Application/Application.hpp
@@ -19,26 +19,73 @@
 #include <vector>
 
 namespace Engine {
+
+    /**
+    * @brief Application and its dependencies
+    *
+    */
     class Application {
         public:
-            // Ctor
+            /**
+             * @brief Create the Application object (Window)
+             *
+             * @param width Width of the window
+             * @param height Height of the window
+             * @param titleWindow Title of the window
+             * @param players Board containing the game support (keyboard or gamepad) of the players. The number of media declared will dictate the number of players
+             *
+             */
             Application(unsigned int width, unsigned int height, const
             std::string &titleWindow, std::vector<std::string> players);
 
-            // Dtor
+            /**
+             * @brief Destroy the Application object
+             *
+             */
             ~Application();
 
-            // Member functions
+            /**
+             * @brief Launch the window
+             *
+             */
             void run();
+
+            /**
+             * @brief Update the content of the window
+             *
+             */
             void update();
 
         private:
-            // Objects
+            /**
+            * @brief A pointer to window functions
+            *
+            */
             std::unique_ptr<Raylib::Window> _window;
+            /**
+            * @brief A pointer to mouse functions
+            *
+            */
             std::unique_ptr<Raylib::Mouse> _mouse;
+            /**
+            * @brief An array of pointers containing the functions of the player's game support
+            *
+            */
             std::vector<std::shared_ptr<Player>> _players;
+            /**
+            * @brief A pointer to resources loaded
+            *
+            */
             std::unique_ptr<Engine::Resources::Loader> _assets;
+            /**
+            * @brief A pointer to the scene manager
+            *
+            */
             std::unique_ptr<SceneManager> _sceneManager;
+            /**
+            * @brief A pointer to the audio manager
+            *
+            */
             std::unique_ptr<AudioManager> _audioManager;
     };
 }
--- includes/Application/Player.hpp
+++ includes/Application/Player.hpp
@@ -12,24 +12,87 @@
 #include "../../libs/Raylib/includes/Gamepad.hpp"
 
 namespace Engine {
+    /**
+    * @brief Player's game support (keyboard or gamepad)
+    *
+    */
     class Player {
         public:
-            // Ctor
+            /**
+             * @brief Initialize the player's game support
+             *
+             * @param inputType The type of support assigned to players (keyboard or gamepad only, other cases are considered as errors)
+             * @param id The id required to identify the controller to the raylib
+             *
+             */
             explicit Player(std::string &inputType, unsigned int id = 0);
 
-            // Dtor
+            /**
+             * @brief Destroy the Player object
+             *
+             */
             ~Player();
 
-            // Member functions
+            /**
+             * @brief Check if the key or button was pressed
+             *
+             * @param keyOrButton code to check
+             * @return true The key is currently pressed
+             * @return false The key isn't currently pressed
+             */
             bool isPressed(int keyOrButton);
+
+            /**
+             * @brief Check if the key or button was down
+             *
+             * @param keyOrButton code to check
+             * @return true The key is currently down
+             * @return false The key isn't currently down
+             */
             bool isDown(int keyOrButton);
+
+            /**
+             * @brief Check if the key or button was released
+             *
+             * @param keyOrButton code to check
+             * @return true The key is currently released
+             * @return false The key isn't currently released
+             */
             bool isReleased(int keyOrButton);
+
+            /**
+             * @brief Check if the key or button was up
+             *
+             * @param keyOrButton code to check
+             * @return true The key is currently up
+             * @return false The key isn't currently up
+             */
             bool isUp(int keyOrButton);
+
+            /**
+             * @brief Get the key or button pressed
+             *
+             * @return ASCII code of the key or button pressed
+             */
             int getPressed();
 
         private:
+            /**
+             * @brief The type of game support
+             *
+             */
             std::string _type;
+
+            /**
+             * @brief A pointer to keyboard functions (null pointer if the game support is "gamepad"
+             *
+             */
             std::unique_ptr<Raylib::Keyboard> _keyboard;
+
+            /**
+             * @brief A pointer to gamepad functions (null pointer if the game support is "keyboard"
+             *
+             */
             std::unique_ptr<Raylib::Gamepad> _gamepad;
     };
 }
